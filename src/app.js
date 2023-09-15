5440915f404b91f76c7a502f14d2ecae7bccb32b
Lisa Heffernan <lisaheffernan@outlook.com>
2023-09-15 14:20:04 +0100

    GET album by id

--- src/app.js
+++ src/app.js
@@ -7,10 +7,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/albums', function (_, res) {
+app.get('/albums', (_, res) => {
   fs.readFile(__dirname + '/' + 'albums.json', 'utf8', function (_, data) {
-    console.log(data)
     res.status(200).send(data)
+  })
+})
+
+app.get('/albums/:id', (req, res) => {
+  fs.readFile(__dirname + '/' + 'albums.json', 'utf8', function (_, data) {
+    const id = req.params.id
+    data = JSON.parse(data)
+    console.log('you requested album ' + id)
+    let returnAlbum
+    for (let i = 0; i < data.length; i++) {
+
+      if (data[i].id == id) {
+        returnAlbum = data[i]
+      }
+    }
+    console.log(returnAlbum)
+    res.status(200).send(JSON.stringify(returnAlbum))
   })
 })
 
